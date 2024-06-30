0c4675d5fefc0816ee0db15b11d9bce026013b15
Brian Acebo <brianacebo@Brians-iMac.attlocal.net>
2024-06-30 13:13:42 -0400

    Updated to new theme files

---
+++ .github/actions/test-action/main.js
@@ -0,0 +1,56 @@
+import core from "@actions/core";
+import github from "@actions/github";
+
+async function run() {
+	try {
+		const source = core.getInput("SOURCE_BRANCH");
+		const pattern = `heads/${core.getInput("TARGET_BRANCH_STARTS_WITH")}`;
+		const token = core.getInput("GITHUB_TOKEN");
+		const {
+			context: {
+				payload: {
+					repository: {
+						name: repoName,
+						owner: { login },
+					},
+				},
+			},
+			GitHub,
+		} = github;
+
+		const octokit = new GitHub(token);
+
+		console.log("Searching by ref", pattern);
+		const { data: targets } = await octokit.git.listMatchingRefs({
+			owner: login,
+			repo: repoName,
+			ref: pattern,
+		});
+
+		if (targets.length === 0) {
+			console.log("No matching branches found");
+		} else {
+			console.log(`Merging to ${targets.length} branches`);
+		}
+
+		await Promise.all(
+			targets.map(async ({ ref }) => {
+				const targetBranch = ref.replace(/^refs\/heads\//, "");
+				console.log(`Beginning merge to ${targetBranch}`);
+				await octokit.repos.merge({
+					owner: login,
+					repo: repoName,
+					base: targetBranch,
+					head: source,
+				});
+				console.log(`Successfully merged ${source} to ${targetBranch}`);
+			})
+		);
+
+		console.log("Done");
+	} catch (error) {
+		core.setFailed(error.message);
+	}
+}
+
+run();
